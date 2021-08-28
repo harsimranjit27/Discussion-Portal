ca315147fe4ff0c34bb5bbccece40edb17b0fd26
harsimranjit27 <harsimran2731@gmail.com>
2021-08-28 18:12:52 +0530

    Fixed issue of wrong questions being appended or no questions(after page refreshing).

--- main.js
+++ main.js
@@ -10,57 +10,54 @@ parentDiv.classList.add("parentDiv");
 let selectedQuestion = document.getElementById("selected_question");
 
 // RESPONSES ADDED (add_response_section)
+let resolve_btn = document.getElementById("resolve");
 let responseSection = document.getElementById("display_responses");
 let responderName = document.getElementById("nameOfResponder");
 let responderComment = document.getElementById("commentOfResponder");
 let submit_response_btn = document.getElementById("submit_response");
 
-// MAIN ARRAY
-let MAIN_ARRAY = [];
-getLocalStorage();
-
-var commentObject;
-
 var idCount = 0;
-localStorage.setItem("id",JSON.stringify(idCount));
+var questionID;
 
-var index;
-var appended = false;
+data = localStorage.getItem("questionsArr");
+if (data) {
+    data = JSON.parse(data);
+}
+
+
+// MAIN ARRAY
+let MAIN_ARRAY = (data || []) ;
+
+MAIN_ARRAY.forEach((arrObject) => {
+    // console.log("arrObj : ", arrObject);
+    // console.log("idCount",idCount);
+    appendParentDivToDOM(arrObject);
+});
 
 submit_ques_btn.addEventListener("click", (event) => {
     
-    idCount = JSON.parse(localStorage.getItem("id"));
-    console.log(idCount);
-
     if (subjectAdded.value !== "" && questionAdded.value !== "") {
-        
+
         var subQuesObject = {
             subjectAdded: subjectAdded.value,
             questionAdded: questionAdded.value,
             id: idCount,
             comments: []
-        }    
-        
+        }
         setLocalStorage(subQuesObject);
         
         appendParentDivToDOM(subQuesObject);
 
         subjectAdded.value = "";
         questionAdded.value = "";
-
-        idCount++;
-        localStorage.setItem("id",JSON.stringify(idCount));
     }
     
 });
 function appendParentDivToDOM(arrObject) {
 
-    idCount = localStorage.getItem("id");
-    
     var childDiv = document.createElement("div");
     childDiv.classList.add("childDiv");
     childDiv.id = idCount;
-    // console.log(childDiv.id);
     
     let subjectH2 = document.createElement("h2");
     subjectH2.innerText = arrObject.subjectAdded;
@@ -75,83 +72,58 @@ function appendParentDivToDOM(arrObject) {
     parentDiv.appendChild(childDiv);
     displayQn.appendChild(parentDiv);
     
+    idCount++;
 }
 
 parentDiv.addEventListener("click", (event) => {
-
+    
     document.querySelector(".add_ques_section").style.display = "none";
     document.querySelector(".add_response_section").style.display = "initial";
 
     MAIN_ARRAY = JSON.parse(localStorage.getItem("questionsArr"));
-
+    
+    // console.log(selectedQuestion);
     while (selectedQuestion.firstChild) {
-       selectedQuestion.removeChild(selectedQuestion.lastChild);
+        selectedQuestion.removeChild(selectedQuestion.lastChild);
     }
-
-    idCount = localStorage.getItem("id");
-
+    
     let selectedQuesToDisplay = document.createElement("div");
-    selectedQuesToDisplay.id = idCount;
-    console.log(selectedQuesToDisplay);
+    selectedQuesToDisplay.id = event.target.parentNode.id;
+    // console.log(selectedQuesToDisplay);
 
-    console.log(event.target.parentNode);
-
+    questionID = event.target.parentNode.id;
+    // console.log("questionID",questionID);
+    
+    // console.log(event.target.parentNode);
+    
     let subjectH2 = document.createElement("h2");
     subjectH2.innerText = MAIN_ARRAY[event.target.parentNode.id].subjectAdded;
     subjectH2.classList.add("subjectH2");
     selectedQuesToDisplay.appendChild(subjectH2);
-
+    
     let questionP = document.createElement("p");
     questionP.innerText = MAIN_ARRAY[event.target.parentNode.id].questionAdded;
     questionP.classList.add("questionP");
     selectedQuesToDisplay.appendChild(questionP);
-
+    
     selectedQuestion.appendChild(selectedQuesToDisplay);
     console.log(selectedQuestion);
-
-    // index = event.target.parentNode.id;
-
-    console.log(idCount);
-
-    showResponsesInDOM(idCount);
-
-});
-
-
-submit_response_btn.addEventListener("click",(event)=>{
-    let rpName = responderName.value;
-    let rpComment = responderComment.value;
-    console.log(rpName);
-    MAIN_ARRAY = JSON.parse(localStorage.getItem("questionsArr"));
-
-    let comment = {
-        name : rpName,
-        comment : rpComment
-    }
-    MAIN_ARRAY[index].comments.push(comment);
-
-    localStorage.setItem("questionsArr",JSON.stringify(MAIN_ARRAY));
     
-    responderName.value = "";
-    responderComment.value = "";
+    showResponsesInDOM(event);
     
-    appendResponsesToDOM(comment);
-
 });
+function showResponsesInDOM(event) {
 
-function showResponsesInDOM(index) {
-
-    console.log(responseSection);
+    // console.log(responseSection);
     if(responseSection.innerHTML){
         responseSection.innerHTML = "";
     }
 
     MAIN_ARRAY = JSON.parse(localStorage.getItem("questionsArr"));
-    idCount =  JSON.parse(localStorage.getItem("id"));
 
-    console.log(MAIN_ARRAY[idCount]);
+    console.log(MAIN_ARRAY[event.target.parentNode.id]);
 
-    MAIN_ARRAY[idCount].comments.forEach(commentArr => {
+    MAIN_ARRAY[event.target.parentNode.id].comments.forEach(commentArr => {
 
         var container = document.createElement("div");
         container.classList.add("responses");
@@ -167,12 +139,31 @@ function showResponsesInDOM(index) {
     });    
 }
 
+submit_response_btn.addEventListener("click",(event)=>{
+
+    let rpName = responderName.value;
+    let rpComment = responderComment.value;
+    console.log(rpName);
+    MAIN_ARRAY = JSON.parse(localStorage.getItem("questionsArr"));
+
+    let comment = {
+        name : rpName,
+        comment : rpComment
+    }
+    MAIN_ARRAY[questionID].comments.push(comment);
+
+    localStorage.setItem("questionsArr",JSON.stringify(MAIN_ARRAY));
+    
+    responderName.value = "";
+    responderComment.value = "";
+    
+    appendResponsesToDOM(comment);
+
+});
 function appendResponsesToDOM(commentArr) {
 
-    MAIN_ARRAY = JSON.parse(localStorage.getItem("questionsArr"));
     if(commentArr.name && commentArr.comment){
 
-        // MAIN_ARRAY[index].comments.forEach(commentArr => {
             let container = document.createElement("div");
             container.classList.add("responses");
             let name = document.createElement("h3");
@@ -183,11 +174,19 @@ function appendResponsesToDOM(commentArr) {
             container.appendChild(name);
             container.appendChild(comment);
             responseSection.appendChild(container);
-        // });
     }
-    
 }
 
+resolve_btn.addEventListener("click",(event)=>{
+
+    parentDiv.removeChild(document.getElementById(questionID));
+    document.querySelector(".add_ques_section").style.display = "initial";
+    document.querySelector(".add_response_section").style.display = "none";
+
+    MAIN_ARRAY.splice(questionID,1);
+    localStorage.setItem("questionsArr",JSON.stringify(MAIN_ARRAY));
+
+});
 
 function setLocalStorage(subQuesObject) {
     
@@ -203,22 +202,5 @@ function setLocalStorage(subQuesObject) {
     console.log(MAIN_ARRAY);
 
     localStorage.setItem("questionsArr", JSON.stringify(MAIN_ARRAY));
-
-}
-
-function getLocalStorage() {
-
-    if (localStorage.getItem("questionsArr") === null) {
-        var MAIN_ARRAY = [];
-    } else {
-        MAIN_ARRAY = JSON.parse(localStorage.getItem("questionsArr"));
-    }
-
-    // console.log(MAIN_ARRAY);
-
-    MAIN_ARRAY.forEach((arrObject) => {
-        // console.log("arrObj : ", arrObject);
-        appendParentDivToDOM(arrObject);
-    });
 
 }
